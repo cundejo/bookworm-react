b93f81291aaabb171cb5a0146dee5cb261eb8e2e
Oliver Sosa <cundejo85@gmail.com>
2017-11-06 09:37:37 -0300

    Starting with redux

--- src/index.js
+++ src/index.js
@@ -2,18 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import rootReducer from './rootReducer';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
-//todo Here, min 4:04 video https://youtu.be/RCPMuJ0zYak?t=4m4s
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
     <BrowserRouter>
-        <App/>
+        <Provider store={store}>
+            <App/>
+        </Provider>
     </BrowserRouter>,
     document.getElementById('root')
 );
